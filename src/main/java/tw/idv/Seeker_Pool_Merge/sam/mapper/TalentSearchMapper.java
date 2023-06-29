b1763dba4239e2c8b648bebfddd2ef4b1bee97c2
might-sam <mack87062966@gmail.com>
2023-06-29 11:56:48 +0800

    06/29 sam update

--- src/main/java/tw/idv/Seeker_Pool_Merge/sam/mapper/TalentSearchMapper.java
+++ src/main/java/tw/idv/Seeker_Pool_Merge/sam/mapper/TalentSearchMapper.java
@@ -9,12 +9,19 @@ import java.util.List;
 
 @Mapper
 public interface TalentSearchMapper {
-public String areas = "";
+
 //    mybatis 用CONCAT連接 '%KEYWORD%'
     @Select("select * from seeker_pool_schemas.member where SK_NO like CONCAT('%', #{keyword}, '%') or MEM_ADDRESS like CONCAT('%', #{keyword}, '%') or MEM_LANG like CONCAT('%', #{keyword}, '%')")
      List<Member> findTalentByKeyword(String keyword);
 
-    List<Member> findByTalentByArea(@Param("areas") String[] areas);  // 寫在xml文件裡
+    @Select("<script>"+
+            " SELECT * FROM seeker_pool_schemas.member " +
+            " WHERE " +
+            "        <foreach item='name' collection='areas' separator=' OR'>\n" +
+            "            MEM_ADDRESS LIKE CONCAT('%', #{name}, '%')\n" +
+            "        </foreach>" +
+            "</script>")
+    List<Member> findTalentByArea(@Param("areas") String[] areas);  // 寫在xml文件裡
 
     @Select("select * from seeker_pool_schemas.member where MEM_ID = #{memId}")
     List<Member> findTalentById(Integer memId);
