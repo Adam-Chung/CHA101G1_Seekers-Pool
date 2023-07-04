d6e5dab5477689670d30ff37e8f21d1f3f042942
Jamie <pinwheel1522@gmail.com>
2023-07-04 14:24:17 +0800

    0704 Jamie update pages

--- src/main/resources/static/front-end/company/companymember/js/send_email.js
+++ src/main/resources/static/front-end/company/companymember/js/send_email.js
@@ -50,59 +50,9 @@ $(function () {
     });
 });
 
-
-// $(function () {
-//     // 寄驗證信
-//     $.ajax({
-//         type: 'POST',
-//         url: '/SeekerPool/RegisterVertification',
-//         success: function (data) {
-//             console.log(data);  // 印出從後端回傳的數據("驗證碼已發送")
-//         },
-//         dataType: 'json'
-//     });
-
-//     $("#confirmBtn").click(function (e) {
-//         e.preventDefault();
-//         console.log($("#checkCodeForm").serialize());  // 印出表單數據
-//         $.ajax({
-//             type: 'POST',
-//             url: '/SeekerPool/RegisterVertification',
-//             data: $("#checkCodeForm").serialize(),
-//             success: function (data) {
-//                 // console.log(data);  // 印出從後端回傳的數據
-//                 if (data.flag) {
-//                     Swal.fire({
-//                         title: '恭喜註冊成功!!',
-//                         timer: 1000,
-//                         icon: 'success',
-//                         width: 400,
-//                         padding: '3em',
-//                         color: '#817be0',
-//                         background: '#fff',
-//                         backdrop: `
-//                                     rgba(0,0,023,0.3)
-//                                     url(https://sweetalert2.github.io/images/nyan-cat.gif)
-//                                     right bottom
-//                                     repeat
-//                                   `
-//                     }).then(() => {
-//                         setTimeout(() => {
-//                             location.href = "companyIndex.html";
-//                         }, 1000)
-//                     })
-//                 } else {
-//                     alert(data.errorMsg);
-//                 }
-//             },
-//             dataType: 'json'
-//         });
-//     });
-// });
-
 // 重新寄驗證碼設定
 function startCountdown() {
-    let timer = 15;
+    let timer = 30;
     const resendBtn = document.getElementById('resendButton');
     const buttonText = resendBtn.innerText;
 
